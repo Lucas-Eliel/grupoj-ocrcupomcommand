6407811e36c984698f118a3b436cb121682366bb
lucaseliel <lucaseliel.business@gmail.com>
2021-12-15 08:33:48 -0300

    Removendo teste local

--- lambda_function.py
+++ lambda_function.py
@@ -3,11 +3,3 @@ from src.controller.ocr_cupom_controller import OcrCupomCommandController
 
 def lambda_handler(event, context):
     return OcrCupomCommandController(event).invoke()
-
-lambda_handler({
-    "resource": "/processamento-ocr-cupom",
-    "httpMethod": "POST",
-    "body": {
-        "cupom": ""
-    }
-}, {})
